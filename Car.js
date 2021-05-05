e7d64eff93b9c179d1d05017174202c73e89d020
cppranger <keksovska@gmail.com>
2021-05-05 14:37:42 +0300

    new things

--- Car.js
+++ Car.js
@@ -1,5 +1,8 @@
 // homework
 class Car {
+
+    static carList = [];
+
     constructor(brand, name, price, weight, fuel_type, color, img) {
         this.brand = brand;
         this.name = name;
@@ -8,11 +11,50 @@ class Car {
         this.fuel_type = fuel_type;
         this.color = color;
         this.img = img;
+        Car.setList(this);
+    }
+    getText() {
+        let out = "";
+        out += `<div class="car">`
+        out += `<div class="car-container">
+                <img src="${this.img}" alt="${this.name}" class="car-img"/>
+                <div class="bottom-left"><p class="brand"><b>${this.brand}</b> ${this.name}</p></div>
+                </div>`;
+        out += `<div class="misc">
+                <ul>
+                    <li>Weight: ${this.weight} kg</li>
+                    <li>Fuel type: ${this.fuel_type}</li>
+                    <li>Color: ${this.color}</li>
+                    <li>Cost: <span class="price">${this.priceCorrector()} ₽</span></li>
+                </ul>
+                </div>`
+        out += `</div>`;
+        return out;
+    }
+    
+    static renderAll(target) {
+        let result = "";
+        Car.carList.forEach(function (car) {
+            result += car.getText();
+        })
+        document.getElementById(target).innerHTML = result;
+    }
+    
+    static setList(car) {
+        return Car.carList.push(car);
+    }
+
+    priceCorrector() {
+        return this.price.toLocaleString();
     }
 }
 
-const tesla = new Car('Tesla', 'Roadster', 20200455, 454, 'electricity', 'red', 'https://avatars.mds.yandex.net/get-zen_doc/48747/pub_5a1155ce77d0e66ae8d7dd53_5a1155d1248090f4a1e452f1/scale_1200');
-const lada = new Car('Lada', 'Kalina', 156870, 1000, 'petrol', 'silver', 'https://s.rdrom.ru/1/pubs/4/23638/1100424.jpg');
+
+
+const tesla = new Car('Tesla', 'Roadster', 20200455, 454, 'electricity', 'red', 'https://i1.sndcdn.com/artworks-000456414930-4l7bci-t500x500.jpg');
+const lada = new Car('Lada', 'Kalina', 156870, 1000, 'petrol', 'red', 'https://akcik.ru/components/com_virtuemart/shop_image/category/_5a1ea559de86b_thumb.jpg');
+const bugatti = new Car('Bugatti', 'Veyron', 170869897, 1000,'petrol', 'blue', 'https://i1.sndcdn.com/avatars-000309463520-5v3bvt-t500x500.jpg')
 
 console.log(tesla);
 console.log(lada);
+Car.renderAll('cars');
--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // template
 class Header {
-    constructor(img, alt, h1, h2) {
+    constructor(img, alt, h1) {
         this.src = img;
         this.alt = alt;
         this.h1 = h1;
-        this.h2 = h2;
     }
 
     render(target) {
         let out = "";
-        out += `<img src="${this.src}" alt="${this.alt}" class="logo"/>`;
-        out += `<h1 class="company-name">${this.h1}</h1>`;
-        out += `<h2 class="auth-info">${this.h2}</h2>`;
+        out += `<div class="site-identity">
+                    <img src="${this.src}" alt="${this.alt}" class="logo"/>
+                    <h1><a href="#">${this.h1}</a></h1>
+                </div>`
+        out += `<nav class="site-navigation">
+                    <ul class="nav">
+                        <li><a href="#">About</a></li> 
+                        <li><a href="#">News</a></li> 
+                        <li><a href="#">Contact</a></li> 
+                    </ul>
+                </nav>`
         document.getElementById(target).innerHTML = out;
     }
 }
 
 const logo = 'https://2.bp.blogspot.com/-J9hheZOCTaI/WNeAKyxjo5I/AAAAAAAAJNE/lSdIsmxsMuEa8gU39LzghFb9u_IVJ4DeACLcB/s1600/ViewDocument.png';
 const alt = logo;
-const h1 = "Hello, user!";
-const h2 = "Please, authorize!";
-let header = new Header(logo, alt, h1, h2);
+const h1 = "Welcome to our small e-shop!";
+let header = new Header(logo, alt, h1);
 header.render('header');
